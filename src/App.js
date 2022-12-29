f0496e99688d323e50da46dc2423f85509e83ca4
grace99 <gracehoneybee740@gmail.com>
2022-12-29 11:42:55 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const Todo = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="list-style">
+      <div className="todo-style">
+        <div>
+          <h2>🥕{props.todo.content}🥕</h2>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      content: "2023년은 토끼의 해",
+    },
+  ]);
+
+  const [content, setContent] = useState("");
+
+  const addHandler = () => {
+    const newTodo = {
+      id: todos.length + 1,
+      content: content,
+    };
+    setTodos([...todos, newTodo]);
+    setContent("");
+  };
+
+  return (
+    <div className="container">
+      <div className="top-input">
+        <input
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="내용을 적어봅시다."
+        />
+        <button className="add-button" onClick={addHandler}>
+          추가하기
+        </button>
+      </div>
+      <div className="title">Todo List 🐰</div>
+      <div className="list-card">
+        {todos.map((todo) => {
+          return <Todo todo={todo} key={todo.id} setTodos={setTodos} />;
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default App;
